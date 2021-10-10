fd20c9aeae5124d78a5f62445a71d2f690ac7f59
Taha Khan <taha1112@gmail.com>
2021-10-10 16:43:52 -0700

    Fixing token for linux systems

--- src/main/java/xyz/tahakhan/badworddetector/auth/TokenGrabber.java
+++ src/main/java/xyz/tahakhan/badworddetector/auth/TokenGrabber.java
@@ -7,12 +7,13 @@ import org.springframework.util.StringUtils;
 @Component
 @Log4j2
 public class TokenGrabber {
+    private static final String TOKEN_KEY = "DiscordBot_BadWordDetector_Token";
     public String getToken() {
         log.info("Obtaining token from env variables");
-        String token = System.getenv("DiscordBot.BadWordDetector.Token");
+        String token = System.getenv(TOKEN_KEY);
         if (!StringUtils.hasText(token))
         {
-            log.error("Unable to obtain token from System Variables, make sure the bot token value is set under the \"DiscordBot.BadWordDetector.Token\" environment variable.");
+            log.error("Unable to obtain token from System Variables, make sure the bot token value is set under the \""+ TOKEN_KEY  + "\" environment variable.");
             throw new IllegalStateException("No token found in environment variables.");
         }
 
