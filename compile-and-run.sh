43ade9784b6c92920c102c987550328d3f775951
Taha Khan <taha1112@gmail.com>
2021-10-10 21:15:29 -0700

    Updating logging levels

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -1,7 +1,12 @@
+#!/bin/sh
 # Create a log directory if it doesn't exist
 if [ ! -d "$HOME/logs" ]; then
   mkdir "$HOME/logs"
 fi
 
+# Get latest
+git pull origin main
+
+# Cleand an build
 mvn clean package spring-boot:repackage
 java -jar target/BadWordDetectorDiscordBot-1.0-SNAPSHOT.jar
